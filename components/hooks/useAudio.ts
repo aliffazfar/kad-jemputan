916696401ce0f9314e98c9c20d5ac07877d1bf92
aliffazfar <aliffazfar34@gmail.com>
2024-11-16 23:16:39 +0800

    update: loop an audio

--- components/hooks/useAudio.ts
+++ components/hooks/useAudio.ts
@@ -1,32 +1,38 @@
-import { useState, useRef, useEffect } from "react";
-import { useMusicStore } from "@/store/music.store";
+import { useState, useRef, useEffect } from 'react'
+import { useMusicStore } from '@/store/music.store'
 
 export const useAudio = () => {
-  const { isPlay, stopMusic, setToggleMusic } = useMusicStore((state) => state);
+  const { isPlay, stopMusic, setToggleMusic } = useMusicStore((state) => state)
 
-  const url = process?.env?.NEXT_PUBLIC_AUDIO ?? "./rc.mp3";
+  const url = process?.env?.NEXT_PUBLIC_AUDIO ?? './rc.mp3'
 
   const musicPlayers = useRef<HTMLAudioElement | undefined>(
-    typeof Audio !== "undefined" ? new Audio(url!) : undefined
-  );
+    typeof Audio !== 'undefined' ? new Audio(url!) : undefined
+  )
 
-  const [audio] = useState(musicPlayers);
+  const [audio] = useState(musicPlayers)
 
-  const toggle = () => setToggleMusic(!isPlay);
-
-  useEffect(() => {
-    isPlay ? audio.current?.play() : audio.current?.pause();
-  }, [isPlay]);
+  const toggle = () => setToggleMusic(!isPlay)
 
   useEffect(() => {
-    audio.current?.addEventListener("ended", () => stopMusic());
+    if (audio.current) {
+      audio.current.loop = true
+    }
+  }, [audio])
+
+  useEffect(() => {
+    isPlay ? audio.current?.play() : audio.current?.pause()
+  }, [isPlay])
+
+  useEffect(() => {
+    audio.current?.addEventListener('ended', () => stopMusic())
     return () => {
-      audio.current?.removeEventListener("ended", () => stopMusic());
-    };
-  }, []);
+      audio.current?.removeEventListener('ended', () => stopMusic())
+    }
+  }, [])
 
   return {
     toggle,
     isPlay,
-  };
-};
+  }
+}
